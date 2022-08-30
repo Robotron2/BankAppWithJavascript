08ebf167a9b7b2907d369853aa0b8e7563fcae25
Theophilus Adesola <theophilusadessola@gmail.com>
2022-08-30 11:49:41 +0100

    updated the function with ES6 syntax

--- bank.js
+++ bank.js
@@ -1,10 +1,20 @@
+const closeNav = () => {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    document.getElementById("mainbtn").style.visibility = "visible";
+}
+const openNav = () => {
+        document.getElementById("mySidebar").style.width = "200px";
+        document.getElementById("main").style.marginLeft = "200px";
+        document.getElementById("mainbtn").style.visibility = "collapse";
+}
+
 error = document.getElementById('disp')
 detailsOfUser = []
 if(localStorage.details){
     detailsOfUser = JSON.parse(localStorage.getItem('details'))
 }
 const register= ()=>{
-    // alert("i dey work")
     if(firstname.value=="" || lastname.value== "" || useremail.value=="" || userpassword.value==""){
         alert("Kindly fill in your details!")
     }
@@ -16,12 +26,10 @@ const register= ()=>{
             email: useremail.value, 
             password: userpassword.value
         }
-        // console.log(userDetails.accNumber)
         detailsOfUser.push(userDetails)
         console.log(detailsOfUser)
         localStorage.setItem('details',JSON.stringify(detailsOfUser))
         alert("Congratulations!, You have Successfully signed Up and your details has been stored.")
-        alert('This is your Account Number: '+ userDetails.accNumber)
         firstname.value = ""
         lastname.value = ""
         useremail.value = ""
@@ -35,19 +43,18 @@ const logIn =()=>{
     if(Email.value == "" || Password.value ==""){
         // alert('Oppss! Kindly fill in your Email and Password')
         error.innerHTML = 'Oppss! Kindly fill in your Email and Password'
-        
     }
     else{
         for(i=0; i<detailsOfUser.length; i++){
             if(detailsOfUser[i].email== Email.value && detailsOfUser[i].password== Password.value){
-                console.log(useremail.value);
                 found = true
                 break;
-            }
-            
+            }  
         }
         if(found){
             alert("Login Successful")
+            window.location.href = "dashboard.html"
+                
         }
         else{
             // alert("Incorrect Email or Password")
