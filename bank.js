5a9dc5c2a4ead034825e110215417c94a33c7331
Love <loveadeniran1@gmail.com>
2022-09-01 14:25:08 +0100

    working on the modal for send money button

--- bank.js
+++ bank.js
@@ -74,7 +74,6 @@ const logIn =()=>{
     }
 }
  // Dashboard
-let initialBalance = 1000
 
  const userDetailsOnLoad =()=>{
     // detailsOfUser = JSON.parse(localStorage.getItem('details'))
@@ -82,52 +81,58 @@ let initialBalance = 1000
     {
         displayNameOfUser.innerHTML=  `${item.firstName},  ${ item.lastName} : ${item.accNumber}`
     })
-
+    allTransferDetails = []
+    if(localStorage.transaction){
+        allTransferDetails = localStorage.getItem('transaction')
+    }
  }
 
 
-allTransferDetails = []
-if(localStorage.transaction){
-    allTransferDetails = localStorage.getItem('transaction')
-}
- const transfer =()=>{
-    transferdetails ={
-        nameOfRecipient : nameofrecipient.value,
-        transferAmount : amountEntered.value,
-        accountNumber: acctNum.value,
-        bankName : bankname.value,
-        mydate : new Date()
+
+let initialBalance = 2000
+ const sendMoney =()=>{
+    if(nameofrecipient.value== "" || amountEntered.value=="" || acctNum.value=="" || bankname.value==""){
+        alert('Oppsss! Empty fields!')
     }
-        
-        if(userInput <= 0){
-            alert("Guy! transfer tangiblle amount")
+    else{
+        transferdetails ={
+            nameOfRecipient : nameofrecipient.value,
+            transferAmount : amountEntered.value,
+            accountNumber: acctNum.value,
+            bankName : bankname.value,
+            mydate : new Date()
         }
-        else if(userInput == ""){
-            alert("Kindly fill in an amount to Transfer!")
-        }
-        else if(userInput > initialBalance){
-            alert("Insufficient Fund! Be Careful!")
-        }
-        else{
-            initialBalance = initialBalance - (parseInt(userInput));
-           alert("Withdrawal Successful!")  
-           displayBalance.innerHTML = "Your current balance is $" + initialBalance
-           amountFromJavascript.innerHTML = initialBalance
-           allTransferDetails.push(transferdetails)
-           localStorage.setItem('transaction',JSON.stringify(allTransferDetails))
-           allTransferDetails.map((item,index)=>{
-            transactionDetails.innerHTML = `
-            <tr>
-                <td>${index+1}</td>
-                <td>${item.nameOfRecipient}</td>
-                <td>${item.accountNumber}</td>
-                <td>${item.transferAmount}</td>
-                <td>${item.mydate.toLocaleTimeString()} ${item.mydate.toLocaleDateString()}</td>
-            </tr>`
-           })
-        }
+            if(amountEntered <= 0){
+                alert("Guy! transfer tangiblle amount")
+            }
+            else if(amountEntered > initialBalance){
+                alert("Insufficient Fund! Be Careful!")
+            }
+            else{
+                initialBalance = initialBalance - (parseInt(amountEntered));
+               alert("Withdrawal Successful!")  
+               displayBalance.innerHTML =  initialBalance
+            //    amountFromJavascript.innerHTML = initialBalance
+               allTransferDetails.push(transferdetails)
+               localStorage.setItem('transaction',JSON.stringify(allTransferDetails))
+               allTransferDetails.map((item,index)=>{
+                
+                transactionDetails.innerHTML = `
+                <tr>
+                    <td>${index+1}</td>
+                    <td>${item.nameOfRecipient}</td>
+                    <td>${item.accountNumber}</td>
+                    <td>${item.transferAmount}</td>
+                    <td>${item.mydate.toLocaleTimeString()} ${item.mydate.toLocaleDateString()}</td>
+                </tr>`
+               })
+            }
     }
    
+    }
+//    const sendMoney =()=>{
+//     alert()
+//    }
 
 
 
