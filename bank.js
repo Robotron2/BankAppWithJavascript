764a5c51592f8c36ad3058c08cd9fc6bc55af92b
Theophilus Adesola <theophilusadessola@gmail.com>
2022-09-01 11:26:04 +0100

    Modal for send money added

--- bank.js
+++ bank.js
@@ -9,6 +9,33 @@ const openNav = () => {
         document.getElementById("mainbtn").style.visibility = "collapse";
 }
 
+// Modal Scripts
+
+var modal = document.getElementById("sendMoneyModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("sendBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
 
 // let regexForEmail = /^(([\a-z])+([@])+([\w])+([.])+([\a-z]{1,5})+([.][\w]{1,5})?)$/
 // const testRegex = ()=>{
